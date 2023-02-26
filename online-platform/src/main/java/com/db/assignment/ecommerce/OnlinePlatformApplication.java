5a898661682b74f78d534d3ac3e94f037e3d86a8
rishi <rishiranjan2003@yahoo.co.in>
2023-02-26 23:34:23 +0530

    initail project setup with dependencies

---
+++ online-platform/src/main/java/com/db/assignment/ecommerce/OnlinePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.db.assignment.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePlatformApplication.class, args);
+	}
+
+}
